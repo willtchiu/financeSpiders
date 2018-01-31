497801de3007998f2b0cee14e8200e9f2927a4fe
Will Chiu <willchiu@wills-mbp.home>
2018-01-31 09:45:24 -0800

    Added mwSpider crawling and article text extraction, semi-clean text

--- financeScraper/financeScraper/spiders/mwSpider.py
+++ financeScraper/financeScraper/spiders/mwSpider.py
@@ -1,18 +1,44 @@
+# DEBUG
 from scrapy.shell import inspect_response
+
 import scrapy
 
 class MWSpider(scrapy.Spider):
     home = 'https://marketwatch.com'
     name = "mws"
-    start_urls = ['https://www.marketwatch.com/investing/stock/aapl/news',
-                  'https://www.marketwatch.com/investing/stock/nvda/news'
+    start_urls = ['https://www.marketwatch.com/investing/stock/aapl/news'#,
+                  #'https://www.marketwatch.com/investing/stock/nvda/news'#
                  ]
-
+    
+    """
     def parse(self, response):
         #inspect_response(response)
 
         for article in response.xpath('//li/div/p/a'):
+            article_link = article.xpath('@href').extract_first()
             yield {
                 'headline': article.xpath('text()').extract_first(),
-                'link': "".join([self.home, article.xpath('@href').extract_first()])
+                'link': "".join([self.home, article_link])
+                'text': yield scrapy.follow(article_link, callback=self.parse_article) if article_link is not None else ""
             }
+    """
+
+    def parse(self, response):
+        for article in response.xpath('//li/div/p/a'):
+            article_link = article.xpath('@href').extract_first()
+            yield response.follow(article_link, callback=self.parse_article) if article_link is not None else ""
+
+    def parse_article(self, response):
+        headline = response.xpath('//title/text()').extract_first()
+        author   = response.xpath('//meta[@name=\'author\']/@content').extract_first()
+
+        raw_text = response.xpath('//p/text()').extract()
+        text     = " ".join(map(str.strip, raw_text))
+
+        return { 'tick': "",
+                 'head': headline,
+                 'author': author,
+                 'link': response.url,
+                 'text': text
+               }
+
